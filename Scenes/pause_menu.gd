678acb74e6a5d80bf56b73abd4bb932649efb0f3
Hoodie227 <4023434@g.highlineschools.org>
2026-06-01 13:25:17 -0700

    wsatvch out brto

--- Scenes/pause_menu.gd
+++ Scenes/pause_menu.gd
@@ -30,4 +30,4 @@ func _on_restart_button_pressed() -> void:
 func _on_quit_button_pressed() -> void:
 	main.play_sfx("select");
 	get_tree().paused = false;
-	get_tree().change_scene_to_file("res://main.tscn");
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn");
